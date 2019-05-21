4118c4ed9de8192c24098d6df443c3fd1e28c718
ll <ll@baitutech.com>
2019-05-21 13:17:48 +0800

    第一次提交

---
+++ GBCocoaPods/DemoColor.h
@@ -0,0 +1,15 @@
+//
+//  DemoColor.h
+//  SimpleDemo
+//
+//  Created by Lee on 2019/5/21.
+//  Copyright © 2019 BT. All rights reserved.
+//
+
+#ifndef DemoColor_h
+#define DemoColor_h
+
+
+#define Demo_Color [UIColor grayColor]
+
+#endif /* DemoColor_h */
